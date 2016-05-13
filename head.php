9a9ec9d7a6ddf10bedd8f2ed3f75bcb79a78fa67
nirocos <nirocos@gmail.com>
2016-05-13 14:36:39 +0700

    file project

---
+++ head.php
@@ -0,0 +1,11 @@
+<head>
+ 	<meta charset="UTF-8">
+ 	<title>webq</title>
+ 	<!-- bootstrap -->
+ 	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+	<!-- iconAwesome -->
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+ </head>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+		if(isset($_POST['username'])){
+			$username = $_POST['username'];
+		}
+		if(isset($_POST['password'])){
+			$password = $_POST['password'];
+		}
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <?php include 'head.php' ?>
+ <body>
+ 	<nav class="navbar navbar-light bg-faded navbar-inverse navbar-fixed-top">
+
+ 		<div class="container-fluid">
+ 			<div class="navbar-header">
+ 				<a class="navbar-brand" href="#">LOGO</a>
+ 			</div>
+ 		</div>
+ 	</nav>
+ </body>
+ </html>
